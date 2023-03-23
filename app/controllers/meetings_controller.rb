e367bfefdc35aecc8913552bd8fcd72673227bc3
Theo <theo.gianella@proton.me>
2023-03-23 10:48:16 +0100

    Implemented basic controller logic and view for meetings#index

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,7 +1,8 @@
 class MeetingsController < ApplicationController
   def index
+    @masterclass = Masterclass.find_by(title: params[:masterclass_title])
+    @meetings = @masterclass.meetings
   end
 
-  def show
-  end
+  def show; end
 end
