ba5ceea8eee135b4d8784771c3fbd26b9d6ddd75
Theo <theo.gianella@proton.me>
2023-03-28 18:02:48 +0200

    Reservation validations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -7,6 +7,13 @@ class Reservation < ApplicationRecord
   after_create :creation_send
   after_update :validation_send
 
+  validates :guest,
+            uniqueness: { scope: :meeting,
+                          message: "un utilisateur ne peut pas s'inscrire deux fois à la même session" }
+  validate :only_one_reservation_per_masterclass_per_user
+  validate :meeting_has_free_spots
+  validate :chef_cannot_book_his_own_meeting
+
   def creation_send
     ChefMailer.reservation_created_email(self).deliver_now
     UserMailer.reservation_created_email(self).deliver_now
@@ -15,5 +22,25 @@ class Reservation < ApplicationRecord
   def validation_send
     ChefMailer.reservation_validated_email(self).deliver_now
     UserMailer.reservation_validated_email(self).deliver_now
+  end
+
+  private
+
+  def only_one_reservation_per_masterclass_per_user
+    return unless guest.attended_masterclasses.include?(meeting.masterclass)
+
+    errors.add(:meeting, "un utilisateur ne peut pas s'inscrire deux fois pour la même masterclass")
+  end
+
+  def meeting_has_free_spots
+    return unless meeting.free_spots <= 0
+
+    errors.add(:meeting, "la session n'a plus de places disponibles")
+  end
+
+  def chef_cannot_book_his_own_meeting
+    return unless guest == meeting.chef
+
+    errors.add(:user, "un chef ne peut pas s'inscrire à sa propre session")
   end
 end
