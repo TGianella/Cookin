f41c209b278e0a9401022a1809b14ca76e7c2b8f
rockethelll <rockethell@hotmail.com>
2023-03-28 10:32:55 +0200

    edit user profile ok

--- app/controllers/guest/profiles_controller.rb
+++ app/controllers/guest/profiles_controller.rb
@@ -10,20 +10,22 @@ class Guest::ProfilesController < ApplicationController
   def update
     @user = User.find_from_param(params[:name])
     puts '*' * 60
-    puts @user
-    puts '*' * 60
     if @user == current_user
       puts '$' * 60
-      @user.update!(user_params)
-      puts '$' * 60
-      flash[:success] = 'Votre profil a bien été mis à jour'
-      redirect_to guest_profile_path(@user)
+      if @user.update!(user_params)
+        puts '$' * 60
+        flash[:success] = 'Votre profil a bien été mis à jour'
+        redirect_to guest_profile_path(@user)
+      else
+        flash[:alert] = "Votre profil n'a pas pu être mis à jour"
+        render :edit
+      end
     end
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :city, :zip_code, :phone_number, :birth_date)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :city, :zip_code, :phone_number, :birth_date)
   end
 end
