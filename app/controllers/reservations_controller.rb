e71358581652a990c1e071655f389742c8bd4bc1
Theo <theo.gianella@proton.me>
2023-03-28 11:02:25 +0200

    Implemented reservation confirmation button for chef

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -16,5 +16,23 @@ class ReservationsController < ApplicationController
     redirect_back fallback_location: root_path
   end
 
+  def update
+    @reservation = Reservation.find(params[:id])
+
+    if current_user == @reservation.meeting.chef && @reservation.status == false
+
+      if @reservation.update(status: true)
+        flash[:success] = "Reservation validée pour #{@reservation.guest.full_name}"
+      else
+        flash[:alert] = "La réservation n'a pas pu être validée"
+      end
+
+    else
+      flash[:error] = "Il n'est pas possible de valider cette réservation"
+    end
+
+    redirect_back fallback_location: root_path
+  end
+
   def destroy; end
 end
