fffa1e06e59c158f31552b35b4b2afa1bc50309f
Theo <theo.gianella@proton.me>
2023-03-29 10:39:46 +0200

    Static sign_in url in mailer

--- app/mailers/chef_mailer.rb
+++ app/mailers/chef_mailer.rb
@@ -3,14 +3,14 @@ class ChefMailer < ApplicationMailer
 
   def reservation_created_email(reservation)
     @reservation = reservation
-    @url = "https://#{HEROKU_APP_NAME}.herokuapp/users/sign_in"
+    @url = 'https://thecookinproject.herokuapp/users/sign_in'
 
     mail(to: @reservation.chef.email, subject: "#{@reservation.guest.full_name} veut réserver pour une session")
   end
 
   def reservation_validated_email(reservation)
     @reservation = reservation
-    @url = "https://#{HEROKU_APP_NAME}.herokuapp/users/sign_in"
+    @url = 'https://thecookinproject.herokuapp/users/sign_in'
 
     mail(to: @reservation.chef.email,
          subject: "Vous avez validé l'inscription de #{@reservation.guest.full_name} pour #{@reservation.masterclass.title} !")
@@ -18,7 +18,7 @@ class ChefMailer < ApplicationMailer
 
   def reservation_cancelled_by_guest_email(reservation)
     @reservation = reservation
-    @url = "https://#{HEROKU_APP_NAME}.herokuapp/users/sign_in"
+    @url = 'https://thecookinproject.herokuapp/users/sign_in'
 
     mail(to: @reservation.chef.email,
          subject: "#{@reservation.guest.full_name} a annulé son inscription pour #{@reservation.masterclass.title}")
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,21 +3,21 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user)
     @user = user
-    @url = "https://#{HEROKU_APP_NAME}.herokuapp/users/sign_in"
+    @url = 'https://thecookinproject.herokuapp/users/sign_in'
 
     mail(to: @user.email, subject: 'Bienvenue sur notre site !')
   end
 
   def reservation_created_email(reservation)
     @reservation = reservation
-    @url = "https://#{HEROKU_APP_NAME}.herokuapp/users/sign_in"
+    @url = 'https://thecookinproject.herokuapp/users/sign_in'
 
     mail(to: @reservation.guest.email, subject: "Votre demande d'inscription pour #{@reservation.masterclass.title}")
   end
 
   def reservation_validated_email(reservation)
     @reservation = reservation
-    @url = "https://#{HEROKU_APP_NAME}.herokuapp/users/sign_in"
+    @url = 'https://thecookinproject.herokuapp/users/sign_in'
 
     mail(to: @reservation.guest.email,
          subject: "Votre inscription est validée pour #{@reservation.masterclass.title} !")
@@ -25,7 +25,7 @@ class UserMailer < ApplicationMailer
 
   def reservation_cancelled_by_guest_email(reservation)
     @reservation = reservation
-    @url = "https://#{HEROKU_APP_NAME}.herokuapp/users/sign_in"
+    @url = 'https://thecookinproject.herokuapp/users/sign_in'
 
     mail(to: @reservation.guest.email,
          subject: "Vous avez annulé votre demande d'inscription pour #{@reservation.masterclass.title}")
@@ -33,7 +33,7 @@ class UserMailer < ApplicationMailer
 
   def reservation_cancelled_by_chef_email(reservation)
     @reservation = reservation
-    @url = "https://#{HEROKU_APP_NAME}.herokuapp/users/sign_in"
+    @url = 'https://thecookinproject.herokuapp/users/sign_in'
 
     mail(to: @reservation.guest.email,
          subject: "Votre demande d'inscription a été annulée #{@reservation.masterclass.title}")
