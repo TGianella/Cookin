77cb8bfcdd00879942377fb4be6ed56421a0a44b
rockethelll <rockethell@hotmail.com>
2023-03-29 16:14:59 +0200

    add chef profile, edit profile, edit password

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,19 +1,33 @@
 class ChefsController < ApplicationController
   def index
     @chefs = User.chefs
+    @chef = User.find_from_param(params[:name])
   end
 
   def show
     @chef = User.find_from_param(params[:name])
   end
 
-  def new; end
+  def edit
+    @chef = User.find_from_param(params[:name])
+  end
 
-  def create; end
+  def update
+    @chef = User.find_from_param(params[:name])
+    if @chef == current_user
+      if @chef.update!(user_params)
+        flash[:success] = 'Votre profil a bien été mis à jour'
+        redirect_to guest_profile_path(@chef)
+      else
+        flash[:alert] = "Votre profil n'a pas pu être mis à jour"
+        render :edit
+      end
+    end
+  end
 
-  def edit; end
+  private
 
-  def update; end
-
-  def destroy; end
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :city, :zip_code, :phone_number, :birth_date)
+  end
 end
