d7ab6c3f0d7e455bf8106a2a2b05bbc48f02019a
Thomas <thomas.kanthack@gmail.com>
2023-03-30 13:05:01 +0200

    test heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,38 +48,42 @@ end
   user.save
 end
 
-# User.chefs.each do |chef|
-#   2.times do |_|
-#     Recipe.create(title: Faker::Food.unique.dish,
-#                   content: Faker::Lorem.paragraph(sentence_count: 50),
-#                   duration: Faker::Number.within(range: 1..36) * 5,
-#                   difficulty: %w[facile moyen difficile].sample,
-#                   chef: chef)
-#   end
+User.chefs.each do |chef|
+  2.times do |_|
+   recipe = Recipe.new(title: Faker::Food.unique.dish,
+                  content: Faker::Lorem.paragraph(sentence_count: 50),
+                  duration: Faker::Number.within(range: 1..36) * 5,
+                  difficulty: %w[facile moyen difficile].sample,
+                  chef: chef)
+    recipe.image.attach(io: File.open("#{Rails.root}/app/assets/images/tajine.png"), filename: 'tajine.png')
+    recipe.save
+  end
 
-#   3.times do |_|
-#     masterclass = Masterclass.new(title: Faker::Restaurant.unique.type,
-#                                   description: Faker::Lorem.paragraph(sentence_count: 20),
-#                                   duration: Faker::Number.within(range: 12..60) * 5,
-#                                   price: Faker::Number.within(range: 1..100),
-#                                   chef: chef)
-#     masterclass.recipes << chef.taught_recipes.sample(rand(1..4))
-#     masterclass.save
-#   end
-# end
+  3.times do |_|
+    masterclass = Masterclass.new(title: Faker::Restaurant.unique.type,
+                                  description: Faker::Lorem.paragraph(sentence_count: 20),
+                                  duration: Faker::Number.within(range: 12..60) * 5,
+                                  price: Faker::Number.within(range: 1..100),
+                                  chef: chef)
 
-# Masterclass.all.each do |masterclass|
-#   rand(1..5).times do |_|
-#     meeting = Meeting.new(masterclass: masterclass,
-#                           start_date: Faker::Date.between(from: DateTime.now, to: 1.year.from_now),
-#                           zip_code: Faker::Address.zip_code,
-#                           capacity: Faker::Number.within(range: 1..10))
-#     meeting.save
-#   end
-# end
+    masterclass.image.attach(io: File.open("#{Rails.root}/app/assets/images/tajine.png"), filename: 'tajine.png')
+    masterclass.recipes << chef.taught_recipes.sample(rand(1..4))
+    masterclass.save
+  end
+end
 
-# User.guests.each do |guest|
-#   Masterclass.all.sample(rand(1..3)).each do |masterclass|
-#     Reservation.create(guest: guest, meeting: masterclass.meetings.sample, status: false)
-#   end
-# end
+Masterclass.all.each do |masterclass|
+  rand(1..5).times do |_|
+    meeting = Meeting.new(masterclass: masterclass,
+                          start_date: Faker::Date.between(from: DateTime.now, to: 1.year.from_now),
+                          zip_code: Faker::Address.zip_code,
+                          capacity: Faker::Number.within(range: 1..10))
+    meeting.save
+  end
+end
+
+User.guests.each do |guest|
+  Masterclass.all.sample(rand(1..3)).each do |masterclass|
+    Reservation.create(guest: guest, meeting: masterclass.meetings.sample, status: false)
+  end
+end
