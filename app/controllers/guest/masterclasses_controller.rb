9ff8a375e74576bca3b331295ce2228bae61225c
steakinzer <clementjodin1u@gmail.com>
2023-03-31 17:52:30 +0200

    bug fix

--- app/controllers/guest/masterclasses_controller.rb
+++ app/controllers/guest/masterclasses_controller.rb
@@ -4,7 +4,7 @@ class Guest::MasterclassesController < ApplicationController
   end
 
   private
-  
+
   def find_past_meetings
     @meetings = current_user.meetings.each_with_object([]) { |meeting, past_meetings| past_meetings << meeting if meeting.start_date < DateTime.now }
   end
--- app/controllers/guest/recipes_controller.rb
+++ app/controllers/guest/recipes_controller.rb
@@ -7,7 +7,7 @@ class Guest::RecipesController < ApplicationController
   end
 
   private
-  
+
   def find_past_meetings
     @meetings = current_user.meetings.each_with_object([]) { |meeting, past_meetings| past_meetings << meeting if meeting.start_date < DateTime.now }
   end
