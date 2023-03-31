34d925a40b23bb181b026de374d1439c963a06b4
Theo <theo.gianella@proton.me>
2023-03-31 18:00:38 +0200

    Protected reservations#index page for chefs

--- app/controllers/chef/reservations_controller.rb
+++ app/controllers/chef/reservations_controller.rb
@@ -1,4 +1,6 @@
 class Chef::ReservationsController < ApplicationController
+  before_action :authenticate_user!, :authenticate_chef!
+
   def index
     @masterclasses = current_user.given_masterclasses
   end
